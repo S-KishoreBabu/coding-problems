718bf23d46dbcc9946b59fca585db23ddd6ddd4e
Kishore Babu <145316609+S-KishoreBabu@users.noreply.github.com>
2025-01-05 23:42:51 +0530

    Create Shifting_letters.c

---
+++ Shifting_letters.c
@@ -0,0 +1,18 @@
+int maxScore(char* s) {
+    int n = strlen(s);
+    int sum,split,max=0;
+    for(int i = 0;i<n;i++){
+        split=i+1;
+        sum=0;
+        for(int j=0;j<n;j++){
+            if(j<split){
+                if(s[j]=='0'){sum++;}
+            }else{
+                if(s[j]=='1'){sum++;}
+            }
+        }
+        if(sum==n){return sum;}
+        if(sum>max){max=sum;}
+    }
+    return max;
+}
