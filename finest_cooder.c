bd765929cc3aa3ad3a0928a919b14f342a25847a
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-01-30 09:31:51 +0530

    Update finest_cooder.c

--- finest_cooder.c
+++ finest_cooder.c
@@ -75,7 +75,7 @@ int totaltime(int *nums, int numsSize){
 }
 
 void display_matrix(){
-    printf("\n\n");
+    printf("\n");
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             printf("%d ", matrix[i][j]);
