8a25081da80a246bc165df1a67a3bbace1e6b465
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-02-26 21:18:46 +0530

    `Removed fcNew4.c file`

---
+++ leetcode-125.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+
+char *removeAlphaNumeric(char *arr){
+    char *string = (char*)malloc(strlen(arr));
+    int j=0;
+    for(int i=0;i<strlen(arr);i++){
+        if(isalpha(arr[i])){
+            if(isupper(arr[i])){
+                string[j]=tolower(arr[i]);
+            }
+            else{
+                string[j]=arr[i];
+            }
+            j++;
+        }
+    }
+    string[j]='\0';
+    return string;
+}
+
+int main() {
+    char *testString = "A man, a plan, a canal: Panama";
+    if (isPalindrome(testString)) {
+        printf("\"%s\" is a palindrome.\n", testString);
+    } else {
+        printf("\"%s\" is not a palindrome.\n", testString);
+    }
+    return 0;
+}
+
+bool isPalindrome(char* s) {
+
+    if(strlen(s)==1){
+        return true;
+    }
+    char *arr=removeAlphaNumeric(s);
+    int j;
+    for(int i=0;i<(strlen(arr)/2);i++){
+        j=(strlen(arr)-i)-1;
+        if(arr[i] != arr[j]){
+            return false;
+        }
+    }
+    return true;
+}
