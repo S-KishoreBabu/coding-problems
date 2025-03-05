5cbd6d3b1bc2fad3d7096304ec2bda182481f326
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-03-06 00:01:46 +0530

    -

--- leetcode-1768.c
+++ leetcode-1768.c
@@ -6,28 +6,27 @@
 
 char* mergeAlternately(char * word1, char * word2){
     int n = strlen(word1)+strlen(word2);
-    char *arr=(char*)malloc((sizeof(char)*n)+1);
+    char *arr=(char*)malloc(n+1);
     printf("arr[0] = %c",arr[0]);
     int j=0;
     int k=0;
     int i=0;
     while(i<n){
-        
         if(j<strlen(word1)){
             arr[i]=word1[j];
             j++;
+            printf("\n arr[%d] = %c",i,arr[i]);
             i++;
         }
-        printf("\n arr[%d] = %c",i,arr[i]);
         if(k<strlen(word2)){
             arr[i]=word2[k];
             k++;
+            printf("\n arr[%d] = %c",i,arr[i]);
             i++;
 
         }
-        printf("\n arr[%d] = %c",i,arr[i]);
-        
     }
+    arr[n]='\0';
     return arr;
 }
 
