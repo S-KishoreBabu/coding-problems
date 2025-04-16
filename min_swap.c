2789409317da78ccf66e4ae10ecd482e2d11e826
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-04-16 20:36:41 +0530

    -

--- min_swap.c
+++ min_swap.c
@@ -1,45 +1,50 @@
-// completed 
+#include<stdio.h>
+#include<stdlib.h>
 
-#include <stdio.h>// completed
-#include <math.h>
-#include <stdlib.h>
-#include <string.h>
+void swap(int,int,int,int);
 
-void findMinSwap(char *arr1, char *arr2, int size) {
-    int swap = 0;
-    for (int i = size - 1; i > 0; i--) {
-        int a = (arr1[i - 1] - '0') * 10 + (arr1[i] - '0');
-        int b = (arr2[i - 1] - '0') * 10 + (arr2[i] - '0');
-        int c = (arr1[i - 1] - '0') * 10 + (arr2[i] - '0');
-        int d = (arr2[i - 1] - '0') * 10 + (arr1[i] - '0');
+int arr[][]= {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+int n = 3;
 
-        printf("\nComparing pairs:\n");
-        printf(" a = %d, b = %d \n c = %d, d = %d\n", a, b, c, d);
-        printf(" |a - b| = %d, |c - d| = %d\n", (a - b), (c - d));
-
-        // Check if a swap is needed based on the differences
-        // if ((a - b < 0 && c - d > 0) || (a - b > 0 && c - d < 0)) {
-        //     if (abs(a - b) == abs(c - d)) {
-        //         swap++;
-        //         printf("Swap condition met: Swapping pairs a-b and c-d\n");
-        //     }
-        // } else if (abs(a - b) > abs(c - d)) {
-        //     swap++;
-        //     printf("Smaller difference found: Swapping to minimize difference\n");
-        // }
-
-        if ((a - b) > (c - d)) {
-            swap++;
-            printf("Smaller difference found: Swapping to minimize difference\n");
-        }
-    }
-    printf("\nTotal number of swaps needed: %d\n", swap);
+void swap(int *a,int *b,int *c,int *d ){
+    int temp = *a;
+    *a = *d;
+    *d = *c;
+    *c = *b;
+    *b = temp;
 }
 
-int main() {
-    char arr1[] = "97567";//30124
-    char arr2[] = "34123";//24897
-    int size = strlen(arr1);
-    findMinSwap(arr1, arr2, size);
+void migrate(int i, int j){
+    swap(
+        &arr[i][j],
+        &arr[i][n-j],
+        &arr[j][i],
+        &arr[n-j][i]
+    );
+}
+void rotate(){
+    for(int i = 0;i<n/2;i++){
+        for(int j = 0;j<n/2;j++){
+            migrate(i,j);
+        }
+    }
+}
+
+void display(){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int main(){
+    display();
     return 0;
 }
