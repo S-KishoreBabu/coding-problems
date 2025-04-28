aa5f6eda8e937d056b1287fed9a4e148f8b11cdb
S-KishoreBabu <s.kishorebabu8@gmail.com>
2025-04-28 23:33:02 +0530

    -

--- pseodobinary.c
+++ pseodobinary.c
@@ -1,35 +1,51 @@
-// sum of minimum possible combination using pseudobinary number
+#include <stdio.h>
 
-#include<stdio.h> // completed
-#include<math.h>
+// Function declaration
+int search(int* nums, int numsSize, int target);
+int findMin(int*, int);
 
-void Pseudobinary(int num){
-    int x,y,no,k,pv;
-    while (num!=0){
-        y=0;
-        no = num;
-        k=0;
-        pv=1;
-        while(no!=0){
-            x = no%10;
-            if(x>0){
-                y=1*pv+y;
-            }
-            else{
-                y=0*pv+y;
-            }
-            no = no/10;
-            k=(k*10)+1;
-            pv = pv *10;
-        }
-        printf("%d + ",y);
-        num = num-y;
+int findMin(int* nums, int numsSize) {
+    int mid,l = 0, r = numsSize -1;
+    while(l+1<r){
+        mid = l+(r-l)/2;
+        if(nums[mid]>nums[r]) l = mid;
+        else r = mid;
     }
+    return (nums[l]<nums[r])?l:r;
 }
-int main(){
-    int n;
-    printf("Enter the number of terms: ");
-    scanf("%d",&n);
-    Pseudobinary(n);
+
+int search(int* nums, int numsSize, int target) {
+    int x,low = 0, high = numsSize-1,sep = findMin(nums,numsSize);
+    if(numsSize==1) return (nums[0]==target)?0:-1;
+    int binarySearch(int left, int right, int target){
+        int mid = -1;
+        while(left <= right){
+            mid = left + (right - left)/2;
+            printf("\n test (mid) = %d",mid);
+            if(nums[mid] == target) return mid;
+            else if(nums[mid]<target) left = mid+1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+    if(sep == 0) return binarySearch(low,high,target);
+    x = binarySearch(low,sep-1,target);
+    printf("\n high = %d",sep);
+    return (x != -1)?x:binarySearch(sep,high,target);
+}
+
+int main() {
+    // Example input
+    int nums[] = {4,5,6,7,0,1,2};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int target = 2;
+
+    // Call your function
+    int result = search(nums, numsSize, target);
+
+    // Print the result
+    printf("\n %d\n", result);
+
     return 0;
 }
+
